f0c0cbb3675392ccd5be85be10da25bf6f21b9b7
Tiffani Burk <greengurl1088@yahoo.com>
2022-06-01 21:59:29 -0500

    arrays code

---
+++ main.js
@@ -0,0 +1,9 @@
+const yellowFruit = "Banana"
+const orangeFruit = "Orange"
+const redFruit = "Apple"
+const greenFruit = "Watermelon"
+const blueFruit = "Blueberry"
+
+const fruits = ['Banana', 'Orange', 'Apple', 'Watermelon', 'Blueberry'];
+
+console.log(fruits)
